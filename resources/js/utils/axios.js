80d6144834190ff47709d9f909b88842e323d127
rainierXcode <rainiersapin0131@gmail.com>
2026-06-10 10:32:06 +0800

    fix: prevent login modal from closing on invalid credentials

--- resources/js/utils/axios.js
+++ resources/js/utils/axios.js
@@ -19,7 +19,7 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
     (response) => response,
     (error) => {
-        if (error.response?.status === 401) {
+        if (error.response?.status === 401 && localStorage.getItem("auth_token")) {
             localStorage.removeItem("auth_token");
             localStorage.removeItem("user");
             window.location.href = "/";
